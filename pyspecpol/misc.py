6f76adcd4c02b434ecafeb455cb95d862eefd687
Heloise <fstevance1@sheffield.ac.uk>
2019-01-25 15:15:10 +0000

    First commit of pyspecpol

---
+++ pyspecpol/misc.py
@@ -0,0 +1,15 @@
+"""
+25 - Jan - 2019 / H.F. Stevance / hfstevance@gmail.com
+
+T'is only the beginning
+"""
+
+from __future__ import division, print_function 
+import sys
+
+if sys.version_info.major < 3:
+    range = xrange
+    input = raw_input
+
+if __name__ == "__main__":
+    print("Hello World!")
